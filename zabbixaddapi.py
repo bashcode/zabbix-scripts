e0fd9e6947b7b48d4dac519bb0ff427f56dc1b6c
M0rph3u5 <divyanshushukl@gmail.com>
2017-01-25 18:29:52 +0530

    Add files via upload

---
+++ zabbixaddapi.py
@@ -0,0 +1,17 @@
+from pyzabbix import ZabbixAPI
+
+zapi = ZabbixAPI("http://zabbix.theserverguy.com/zabbix")
+zapi.login("admin", "divyanshu@theserverguy.com")
+print("Connected to Zabbix API Version %s" % zapi.api_version())
+for hostids in zapi.host.create({ 
+'host': "testpythonapi", 
+'interfaces':[{
+'type': "1", 
+'main': "1", 
+'useip': "1", 
+'dns' : "2.2.2.2",
+'ip' : "1.1.1.1",
+'port' : "10050",}],
+'groups': [{"groupid":"2"}], 
+'templates' : [{ "templateid":"10094"}]}):
+    print "succesfull"
---
+++ zabbixdeleteapi.py
@@ -0,0 +1,6 @@
+from pyzabbix import ZabbixAPI
+zapi = ZabbixAPI("http://zabbix.theserverguy.com/zabbix")
+zapi.login("admin", "divyanshu@theserverguy.com")
+print("Connected to Zabbix API Version %s" % zapi.api_version())
+for h in zapi.host.delete("10112") :
+    print "successfull"
