359a581d8e4b72ba050ba87ef2a38b0c74889981
D1vy4n5hu 5hukl4 <divyanshushukl@gmail.com>
2017-02-04 18:07:10 +0530

    Delete zabbixdeleteapi.py

--- zabbixdeleteapi.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python
-from pyzabbix import ZabbixAPI
-zapi = ZabbixAPI("http://zabbix.theserverguy.com/zabbix")
-zapi.login("admin", "divyanshu@theserverguy.com")
-print("Connected to Zabbix API Version %s" % zapi.api_version())
-for h in zapi.host.delete("10112") :
-    print "successfull"
